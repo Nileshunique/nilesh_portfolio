0b5f9f1a31a710a8dcb8f7c56134c3c8fbb9955a
Nilesh Kumar <nilesh.ignou15@gmail.com>
2025-05-30 22:50:11 +0530

    about page updated for the description

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,33 +1,82 @@
 import Heading from "../../SharedComponent/Heading/Heading";
-import apaapi from "./../../assets/images/icons/apaapi_logo.png";
-import bedrock from "./../../assets/images/icons/bedrock.svg";
-import { SiRedux, SiJavascript, SiExpress, SiMongodb } from "react-icons/si";
-import {
-  FaUbuntu,
-  FaReact,
-  FaHtml5,
-  FaCss3,
-  FaNodeJs,
-  FaGitAlt,
-} from "react-icons/fa";
 import { handleNavigate } from "../../utils";
+import profile from "./../../assets/images/profile-small.png";
+import {
+  aboutProfileHeading,
+  aboutProfileList,
+  services,
+  skills,
+} from "../../constant/aboutConstant";
 
 function About() {
   return (
     <div id="About" className=" text-white p-8 font-sans">
       <section className="mb-12">
-        <Heading text="About" borderHeight={"h-9"} />
-        <div className="flex">
-          <div className="w-1 bg-yellow-400 mr-4 flex-shrink-0"></div>
-          <p className="text-gray-300 leading-relaxed">
-            I’m a Full Stack Developer with a passion for transforming ideas
-            into dynamic web applications. Proficient in both front-end
-            technologies like React and back-end frameworks such as Node.js, I
-            focus on creating seamless user experiences. With a strong
-            problem-solving mindset and a commitment to continuous learning, I
-            thrive on collaboration and am always eager to tackle new
-            challenges.
-          </p>
+        <Heading text={aboutProfileHeading} borderHeight={"h-9"} />
+        <div className="flex mb-4 gap-4 flex-wrap lg:flex-nowrap">
+          <div className="rounded-lg mx-auto max-w-md ">
+            <img
+              src={profile}
+              alt="Ivan Dan"
+              className="rounded-lg  hover:drop-shadow-border relative"
+            />
+          </div>
+          <div className="flex-1 flex flex-col justify-center sm:min-w-[400px]">
+            <div className="flex">
+              <div className="w-1 bg-yellow-400 mr-4 flex-shrink-0"></div>
+              <p className="text-gray-300 leading-relaxed">
+                I’m a
+                <span className="text-yellow-400"> Full Stack Developer </span>
+                with a passion for transforming ideas into dynamic web
+                applications. Proficient in both front-end technologies like
+                ReactJS and back-end frameworks such as Node.js, I focus on
+                creating seamless user experiences. With a strong
+                problem-solving mindset and a commitment to continuous learning,
+                I thrive on collaboration and am always eager to tackle new
+                challenges.
+              </p>
+            </div>
+            <div class="text-gray-100 p-6">
+              <ul class="space-y-3">
+                {aboutProfileList.map((item, index) => (
+                  <li key={index} class="flex">
+                    <span class="min-w-24 md:min-w-48 font-semibold text-gray-400">
+                      {item.title}
+                    </span>
+                    <span>: {item.value}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="mb-8">
+        <Heading text="Service" borderHeight={"h-9"} />
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-center">
+          {services.map((service, index) => (
+            <div
+              key={index}
+              className="bg-neutral-900 hover:bg-neutral-700 p-4 rounded-lg flex flex-col gap-2 items-center justify-center border border-solid border-neutral-700 transition-transform hover:scale-105"
+            >
+              <div className={`text-4xl mb-2 ${service.color} rounded-md p-2`}>
+                {service.type === "image" ? (
+                  <img
+                    src={service.icon}
+                    alt={service.title}
+                    className="h-16 p-1"
+                  />
+                ) : (
+                  service.icon
+                )}
+              </div>
+              <h3 className="text-lg font-semibold">{service.title}</h3>
+              <p className="text-sm text-gray-400 text-justify">
+                {service.description}
+              </p>
+            </div>
+          ))}
         </div>
       </section>
 
@@ -60,109 +109,3 @@ function About() {
 }
 
 export default About;
-
-const skills = [
-  {
-    name: "ReactJS",
-    icon: <FaReact />,
-    type: "icon",
-    color: "bg-blue-500",
-    rating: "4",
-    link: "https://react.dev/",
-  },
-  {
-    name: "Redux",
-    icon: <SiRedux />,
-    type: "icon",
-    color: "bg-blue-500",
-    rating: "4",
-    link: "https://redux-toolkit.js.org/",
-  },
-  // {
-  //   name: "MUI",
-  //   icon: <SiMui />,
-  //   type: "icon",
-  //   color: "bg-cyan-400",
-  //   rating: "4",
-  // },
-  {
-    name: "JavaScript",
-    icon: <SiJavascript />,
-    type: "icon",
-    color: "bg-yellow-400",
-    rating: "4",
-    link: "https://javascript.info/",
-  },
-  {
-    name: "HTML",
-    icon: <FaHtml5 />,
-    type: "icon",
-    color: "bg-yellow-400",
-    rating: "4",
-    link: "https://www.w3schools.com/html/",
-  },
-  {
-    name: "CSS",
-    icon: <FaCss3 />,
-    type: "icon",
-    color: "bg-yellow-400",
-    rating: "4",
-    link: "https://www.w3schools.com/css/default.asp",
-  },
-  {
-    name: "NodeJS",
-    icon: <FaNodeJs />,
-    type: "icon",
-    color: "bg-green-400",
-    rating: "4",
-    link: "https://nodejs.org/",
-  },
-  {
-    name: "ExpressJS",
-    icon: <SiExpress />,
-    type: "icon",
-    color: "bg-green-400",
-    rating: "4",
-    link: "https://expressjs.com/",
-  },
-  {
-    name: "MongoDB",
-    icon: <SiMongodb />,
-    type: "icon",
-    color: "bg-green-400",
-    rating: "4",
-    link: "https://www.mongodb.com/",
-  },
-  {
-    name: "Git Source Control",
-    icon: <FaGitAlt />,
-    type: "icon",
-    color: "bg-red-500",
-    rating: "4",
-    link: "https://git-scm.com/",
-  },
-  {
-    name: "Ubuntu",
-    icon: <FaUbuntu />,
-    type: "icon",
-    color: "bg-red-500",
-    rating: "4",
-    link: "https://ubuntu.com/",
-  },
-  {
-    name: "Amazon Bedrock",
-    icon: bedrock,
-    type: "image",
-    color: "bg-yellow-500",
-    rating: "4",
-    link: "https://aws.amazon.com/bedrock/",
-  },
-  {
-    name: "Amazon PAAPI Integration",
-    icon: apaapi,
-    type: "image",
-    color: "bg-white-500",
-    rating: "4",
-    link: "https://affiliate-program.amazon.in/",
-  },
-];
