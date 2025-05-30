981927a57f6c49803c6fb94e6bb65c45490d994a
Nilesh Kumar <nilesh.ignou15@gmail.com>
2025-05-30 22:51:22 +0530

    experiance updated

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,40 +1,101 @@
+import {
+  achievements,
+  education,
+  experience,
+} from "../../constant/experienctConstant";
 import Heading from "../../SharedComponent/Heading/Heading";
 
 const Experience = () => {
   return (
-    <div id="Experience" className="bg-neutral-900 text-white py-8 px-8">
-      <Heading text="Experience" borderHeight={"h-9"} />
-      <div className="ml-6">
-        <div className="mb-12">
-          <div className="flex h-full">
-            <div className="bg-yellow-400 w-4 h-4 rounded-full mr-2 -ml-[0.45rem] mt-2 mb-2 drop-shadow-glow-yellow"></div>
-            <div className="h-full">
-              <h3 className="text-xl font-semibold">MERN Stack Developer</h3>
-              <p className="text-gray-400">Oodles Technology</p>
-            </div>
-          </div>
-          <div className="border-l-2 border-yellow-400 pl-4">
-            <div className="tems-center mb-4">
-              <p className="text-gray-400">Since May 2022</p>
-            </div>
-            <p>
-              {"I'm"} an experienced Frontend Developer with expertise in
-              ReactJS and a deep understanding of the latest technologies. I
-              have practical experience working with tools and frameworks such
-              as JavaScript, Material UI, React-Bootstrap, and Redux-Toolkit. I
-              have made significant contributions to projects such as Heartbeat
-              Design, Plant Tagging, ReBuilding, EngageAI and KaboodleAI by
-              utilizing my expertise to develop innovative solutions that meet
-              project requirements. I ensures that the frontend of the projects
-              I works on is intuitive and user-friendly. {"I'm"} committed to
-              enhancing my skills and knowledge by staying up-to-date with
-              industry trends to deliver the best possible results.
-            </p>
-          </div>
+    <section>
+      <div id="Experience" className="bg-neutral-900 text-white py-8 px-8">
+        <Heading text="Experience" borderHeight={"h-9"} />
+        <div className="ml-6">
+          {experience.map((exp, index) => (
+            <EachItem
+              key={index}
+              title={exp.title}
+              institution={exp.company}
+              duration={exp.duration}
+              description={exp.description}
+              image={exp.image || ""}
+            />
+          ))}
         </div>
       </div>
-    </div>
+
+      <div className="bg-neutral-900 text-white py-8 px-8">
+        <Heading text="Education" borderHeight={"h-9"} />
+        <div className="ml-6">
+          {education.map((edu, index) => (
+            <EachItem
+              key={index}
+              title={edu.title}
+              institution={edu.institution}
+              duration={edu.duration}
+              description={edu.description}
+              image={edu.image || ""}
+            />
+          ))}
+        </div>
+      </div>
+
+      {/* <div className="bg-neutral-900 text-white py-8 px-8">
+        <Heading text="Achievements" borderHeight={"h-9"} />
+        <div className="ml-6">
+          {achievements.map((ach, index) => (
+            <EachItem
+              key={index}
+              title={ach.title}
+              institution={ach.institution}
+              duration={ach.duration}
+              description={ach.description}
+              image={ach.image || ""}
+            />
+          ))}
+        </div>
+      </div> */}
+    </section>
   );
 };
 
 export default Experience;
+
+const EachItem = ({
+  title,
+  institution,
+  duration,
+  description,
+  image = "",
+}) => {
+  return (
+    <div className="mb-6 flex justify-between flex-row flex-wrap md:flex-nowrap">
+      <div className="flex flex-col">
+        <div className="flex h-full">
+          <div className="bg-yellow-400 w-4 h-4 rounded-full mr-2 -ml-[0.45rem] mt-2 mb-2 drop-shadow-glow-yellow"></div>
+          <div className="h-full">
+            <h3 className="text-xl font-semibold">{title}</h3>
+            <p className="text-gray-400">{institution}</p>
+          </div>
+        </div>
+        <div className="border-l-2 border-yellow-400 pl-4">
+          {duration && (
+            <div className="items-center mb-4">
+              <p className="text-gray-400">{duration}</p>
+            </div>
+          )}
+          {description && <p>{description}</p>}
+        </div>
+      </div>
+      {image && (
+        <div className="flex-shrink-0 ml-4">
+          <img
+            src={image}
+            alt={title}
+            className="h-44 object-cover rounded-lg shadow-lg"
+          />
+        </div>
+      )}
+    </div>
+  );
+};
